edad8b6d3438437266d67d2bae7d235a65072631
Varun Balawade <varunbalawade@gmail.com>
2025-04-23 18:02:41 +0530

    Init

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+// Networking
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Std lib
+#include <algorithm>
+#include <format>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+
+
+int main(int argc, char** argv) {
+    std::cout << "Hello world\n";
+    return 0;
+}
