3136d6a3bdd530998a2e6a51b3f629cc0dbba461
Varun Balawade <varunbalawade@gmail.com>
2025-05-06 19:11:39 +0530

    Add flag to determine whether server is running, and a shutdown mechanism
    Server currently shuts down when it receives a message with the header
    "CloseServer: true", this behavior will be changed later

--- include/HttpServer.hpp
+++ include/HttpServer.hpp
@@ -1,11 +1,15 @@
 #ifndef KNOTS_HTTPSERVER_HPP
 #define KNOTS_HTTPSERVER_HPP
 
+#include <thread>
+
 #include "Router.hpp"
 #include "Socket.hpp"
 
 class HttpServer {
 private:
+    std::atomic<bool> m_isRunning;
+
     sockaddr_in m_address;
     int m_addrlen;
     Socket m_serverSocket;
