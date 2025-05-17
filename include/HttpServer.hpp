5166aa70a0c1c35675d583b3e499b1e849ed6411
Varun Balawade <varunbalawade@gmail.com>
2025-05-17 18:22:44 +0530

    Implement graceful server shutdown in HttpServer with console input
    Additional Changes:
    - Sending "CloseServer: true" header no longer shuts down the server,
      this was meant purely for testing
    - Cleaned up HttpServer::HandleRequest()
    - Organized HttpServer.hpp

--- include/HttpServer.hpp
+++ include/HttpServer.hpp
@@ -3,6 +3,7 @@
 
 #include <arpa/inet.h>
 #include <mutex>
+#include <set>
 #include <thread>
 
 #include "Router.hpp"
@@ -13,28 +14,42 @@
 
 class HttpServer {
 private:
+    // Determine whether server is active or not
     std::atomic<bool> m_isRunning;
 
+    // Hold all active client sockets
+    std::mutex m_activeClientSocketsMutex;
+    std::set<int> m_activeClientSockets;
+
+    // Server information
     sockaddr_in m_address;
     int m_addrlen;
     Socket m_serverSocket;
     int m_serverPort;
 
+    // Router
     Router m_router;
 
+    // Thread Pool
     ThreadPool m_threadPool;
     std::mutex m_threadPoolMutex;
 
+    // Miscellaneous
+    void HandleConsoleInput();
+    void Shutdown();
+    
+    // Handle client connection
+    bool SetClientSocketOptions(const Socket& clientSocket) const;
     void HandleConnection(Socket clientSocket);
     bool HandleRequest(std::stringstream& ss, const Socket& clientSocketFD);
 
+    // Error handling
     void HandleError(const int statusCode, const HttpRequest& req, const Socket& clientSocket) const;
     
-    bool SetClientSocketOptions(const Socket& clientSocket) const;
     
 public:
-    ~HttpServer();
     explicit HttpServer(HttpServerConfiguration& config);
+    ~HttpServer();
 
     void AcceptConnections();
 };
