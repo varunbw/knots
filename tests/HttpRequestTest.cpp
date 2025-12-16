46d05ce5e0c541b8b852452a0a6640f31d3612ca
varunbw <varunbalawade@gmail.com>
2025-12-16 19:45:00 +0530

    Separate `HttpMessageTest` tests into their own respective files

--- tests/HttpRequestTest.cpp
+++ tests/HttpRequestTest.cpp
@@ -2,14 +2,158 @@
 #include "HttpMessage.hpp"
 
 /*
-    @brief Test suite for the HttpResponse class
-    @note This test suite is not exhaustive, but it covers the most important cases
-
-    - ParseHttpRequest
-    - ParseStartLine
-    - ParseHeaders
-    - ParseBody
+    @brief Check default constructor of HttpRequest
 */
+TEST(HttpRequestTest, DefaultConstructor) {
+    
+    HttpRequest req;
+
+    EXPECT_EQ(req.method, HttpMethod::DEFAULT_INVALID);
+    EXPECT_EQ(req.requestUrl, "");
+    EXPECT_EQ(req.version, HttpVersion::DEFAULT_INVALID);
+    
+    EXPECT_EQ(req.headers.size(), 0);
+    
+    EXPECT_EQ(req.body, "");
+}
+
+/*
+    @brief Check parameterized constructor of HttpRequest
+*/
+TEST(HttpRequestTest, ParameterizedConstructor) {
+    
+    HttpRequest req(
+        HttpMethod::POST, 
+        "/test", 
+        HttpVersion::HTTP_1_1, 
+        {{"Host", "localhost:8686"}}, 
+        "body",
+        {{}},
+        {{}}
+    );
+    
+    EXPECT_EQ(req.method, HttpMethod::POST);
+    EXPECT_EQ(req.requestUrl, "/test");
+    EXPECT_EQ(req.version, HttpVersion::HTTP_1_1);
+    
+    EXPECT_EQ(req.headers.size(), 1);
+    EXPECT_EQ(req.GetHeader("Host"), "localhost:8686");
+    
+    EXPECT_EQ(req.body, "body");
+}
+
+/*
+    @brief Check that the HttpRequest object can handle case insensitive headers
+*/
+TEST(HttpRequestTest, CaseInsensitiveHeader) {
+    
+    HttpRequest req(
+        HttpMethod::GET, 
+        "/test", 
+        HttpVersion::HTTP_1_1, 
+        {
+            {"Host", "localhost:8686"},
+            {"Connection", "keep-alive"}
+        }, 
+        "body",
+        {{}},
+        {{}}
+    );
+
+    EXPECT_EQ(req.GetHeader("Host"), "localhost:8686");
+    EXPECT_EQ(req.GetHeader("host"), "localhost:8686");
+    EXPECT_EQ(req.GetHeader("HOST"), "localhost:8686");
+    EXPECT_EQ(req.GetHeader("hOsT"), "localhost:8686");
+    
+    EXPECT_EQ(req.GetHeader("Connection"), "keep-alive");
+    EXPECT_EQ(req.GetHeader("connection"), "keep-alive");
+    EXPECT_EQ(req.GetHeader("CONNECTION"), "keep-alive");
+    EXPECT_EQ(req.GetHeader("cOnNeCtIoN"), "keep-alive");
+
+    EXPECT_EQ(req.headers.size(), 2);
+}
+
+/*
+    @brief Check that all forms of parameter parsing is working properly
+
+    Checks for
+    - No queryParams
+    - Valid single param (param=value)
+    - Valid multiple queryParams (param1=value1&param2=value2)
+    - Empty param value (param=)
+    - Empty param value without '=' (param)
+    - Trailing '?'
+    - Trailing '&'
+    - Mixed
+*/
+TEST(HttpRequestTest, ParameterParsing) {
+
+    // Test simple URL without parameters
+    HttpRequest req1;
+    req1.requestUrl = "/test";
+    EXPECT_EQ(req1.requestUrl, "/test");
+    EXPECT_EQ(req1.queryParams.size(), 0);
+
+    // Test URL with single parameter
+    HttpRequest req2;
+    std::stringstream ss2("GET /page?param=value HTTP/1.1\r\n\r\n");
+    req2.ParseFrom(ss2);
+    EXPECT_EQ(req2.requestUrl, "/page");
+    EXPECT_EQ(req2.queryParams.size(), 1);
+    EXPECT_EQ(req2.queryParams["param"], "value");
+
+    // Test URL with multiple parameters
+    HttpRequest req3;
+    std::stringstream ss3("GET /index.html?a=1&b=2&c=3 HTTP/1.1\r\n\r\n");
+    req3.ParseFrom(ss3);
+    EXPECT_EQ(req3.requestUrl, "/index.html");
+    EXPECT_EQ(req3.queryParams.size(), 3);
+    EXPECT_EQ(req3.queryParams["a"], "1");
+    EXPECT_EQ(req3.queryParams["b"], "2");
+    EXPECT_EQ(req3.queryParams["c"], "3");
+
+    // Test URL with empty parameter value
+    HttpRequest req4;
+    std::stringstream ss4("GET /search?q= HTTP/1.1\r\n\r\n");
+    req4.ParseFrom(ss4);
+    EXPECT_EQ(req4.requestUrl, "/search");
+    EXPECT_EQ(req4.queryParams.size(), 1);
+    EXPECT_EQ(req4.queryParams["q"], "");
+
+    // Test URL with parameter without value
+    HttpRequest req5;
+    std::stringstream ss5("GET /toggle?dark HTTP/1.1\r\n\r\n");
+    req5.ParseFrom(ss5);
+    EXPECT_EQ(req5.requestUrl, "/toggle");
+    EXPECT_EQ(req5.queryParams.size(), 1);
+    EXPECT_EQ(req5.queryParams["dark"], "");
+
+    // Test URL with trailing question mark
+    HttpRequest req6;
+    std::stringstream ss6("GET /page? HTTP/1.1\r\n\r\n");
+    req6.ParseFrom(ss6);
+    EXPECT_EQ(req6.requestUrl, "/page");
+    EXPECT_EQ(req6.queryParams.size(), 0);
+
+    // Test URL with trailing ampersand
+    HttpRequest req7;
+    std::stringstream ss7("GET /page?a=1& HTTP/1.1\r\n\r\n");
+    req7.ParseFrom(ss7);
+    EXPECT_EQ(req7.requestUrl, "/page");
+    EXPECT_EQ(req7.queryParams.size(), 1);
+    EXPECT_EQ(req7.queryParams["a"], "1");
+
+    // Test URL with mixed parameter types
+    HttpRequest req8;
+    std::stringstream ss8("GET /complex?a=1&b=&c&d=4 HTTP/1.1\r\n\r\n");
+    req8.ParseFrom(ss8);
+    EXPECT_EQ(req8.requestUrl, "/complex");
+    EXPECT_EQ(req8.queryParams.size(), 4);
+    EXPECT_EQ(req8.queryParams["a"], "1");
+    EXPECT_EQ(req8.queryParams["b"], "");
+    EXPECT_EQ(req8.queryParams["c"], "");
+    EXPECT_EQ(req8.queryParams["d"], "4");
+}
 
 /*
     @brief Parse a valid HTTP request
--- tests/HttpResponseTest.cpp
+++ tests/HttpResponseTest.cpp
@@ -76,3 +76,100 @@ TEST(HttpResponseTest, ResponseCode_404) {
     // Body
     EXPECT_EQ(res.body, body);
 }
+
+/*
+    @brief Check default constructor of HttpResponse
+*/
+TEST(HttpResponseTest, DefaultConstructor) {
+
+    HttpResponse res;
+    
+    EXPECT_EQ(res.version, HttpVersion::HTTP_1_1);
+    EXPECT_EQ(res.statusCode, 200);
+    EXPECT_EQ(res.statusText, "OK");
+    
+    EXPECT_EQ(res.headers.size(), 0);
+    EXPECT_EQ(res.body, "");
+}
+
+/*
+    @brief Check parameterized constructor of HttpResponse
+*/
+TEST(HttpResponseTest, ParameterizedConstructor) {
+    
+    HttpResponse res(
+        HttpVersion::HTTP_1_1, 
+        200, 
+        "OK", 
+        {{"Content-Type", "text/html"}}, 
+        "<html></html>"
+    );
+    
+    EXPECT_EQ(res.version, HttpVersion::HTTP_1_1);
+    EXPECT_EQ(res.statusCode, 200);
+    EXPECT_EQ(res.statusText, "OK");
+    
+    EXPECT_EQ(res.headers.size(), 1);
+    EXPECT_EQ(res.GetHeader("Content-Type"), "text/html");
+    
+    EXPECT_EQ(res.body, "<html></html>");
+}
+
+/*
+    @brief Check that the HttpResponse object can handle case insensitive headers
+*/
+TEST(HttpResponseTest, CaseInsensitiveHeader) {
+    
+    HttpResponse res(
+        HttpVersion::HTTP_1_1, 
+        200, 
+        "OK", 
+        {
+            {"Content-Type", "text/html"},
+            {"Connection", "keep-alive"}
+        }, 
+        "<html></html>"
+    );
+
+    EXPECT_EQ(res.GetHeader("Content-Type"), "text/html");
+    EXPECT_EQ(res.GetHeader("content-type"), "text/html");
+    EXPECT_EQ(res.GetHeader("CONTENT-TYPE"), "text/html");
+    EXPECT_EQ(res.GetHeader("CoNtEnT-TyPe"), "text/html");
+
+    EXPECT_EQ(res.GetHeader("Connection"), "keep-alive");
+    EXPECT_EQ(res.GetHeader("connection"), "keep-alive");
+    EXPECT_EQ(res.GetHeader("CONNECTION"), "keep-alive");
+    EXPECT_EQ(res.GetHeader("cOnNeCtIoN"), "keep-alive");
+
+    EXPECT_EQ(res.headers.size(), 2);
+}
+
+// -- Formatters
+
+/*
+    @brief Formatter for HttpRequest
+    This test checks if the HttpRequest object can be formatted correctly
+*/
+TEST(HttpResponseTest, HttpMethod_Formatter) {
+    EXPECT_EQ(std::format("{}", HttpMethod::GET),             "GET");
+    EXPECT_EQ(std::format("{}", HttpMethod::HEAD),            "HEAD");
+    EXPECT_EQ(std::format("{}", HttpMethod::POST),            "POST");
+    EXPECT_EQ(std::format("{}", HttpMethod::PUT),             "PUT");
+    EXPECT_EQ(std::format("{}", HttpMethod::DELETE),          "DELETE");
+    EXPECT_EQ(std::format("{}", HttpMethod::CONNECT),         "CONNECT");
+    EXPECT_EQ(std::format("{}", HttpMethod::OPTIONS),         "OPTIONS");
+    EXPECT_EQ(std::format("{}", HttpMethod::TRACE),           "TRACE");
+    EXPECT_EQ(std::format("{}", HttpMethod::PATCH),           "PATCH");
+    EXPECT_EQ(std::format("{}", HttpMethod::DEFAULT_INVALID), "INVALID");
+}
+
+/*
+    @brief Formatter for HttpResponse
+    This test checks if the HttpResponse object can be formatted correctly
+*/
+TEST(HttpResponseTest, HttpVersion_Formatter) {
+    EXPECT_EQ(std::format("{}", HttpVersion::HTTP_1_0), "HTTP/1.0");
+    EXPECT_EQ(std::format("{}", HttpVersion::HTTP_1_1), "HTTP/1.1");
+    EXPECT_EQ(std::format("{}", HttpVersion::HTTP_2_0), "HTTP/2.0");
+    EXPECT_EQ(std::format("{}", HttpVersion::DEFAULT_INVALID), "INVALID");
+}
