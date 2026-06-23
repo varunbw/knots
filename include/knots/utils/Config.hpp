0bc61e878d3c65120ecca8b1c3aba8f6f148095f
varunbw <varunbalawade@gmail.com>
2026-06-23 17:01:01 +0530

    Add comments for `RequestLoggingVerbosity`

--- include/knots/utils/Config.hpp
+++ include/knots/utils/Config.hpp
@@ -1,5 +1,28 @@
 #pragma once
 
+/*
+    Specify the verbosity at which the server should log a req-res combo
+
+    1. NONE
+        Quite literally that
+
+    2. BASIC
+        Res Code, Req Method, "Req URL"
+        ex: 200 GET "/index.html"
+
+    3. INCLUDE_IP
+        Add IP to BASIC
+        ex: 127.0.0.1       - 200 GET "/index.html"
+        The padding is meant to accomodate all IPs, so everything is in a neat line
+
+    4. INCLUDE_TIME
+        Add Date & Time info to BASIC
+        ex: [2026-06-23 16:55:07.48] - 200 GET "/index.html"
+
+    5. FULL
+        Everything
+        ex: [2026-06-23 16:54:25.24] 127.0.0.1       - 200 GET "/index.html"
+*/
 enum class RequestLoggingVerbosity {
     NONE,
     BASIC,
@@ -13,6 +36,7 @@ enum class RequestLoggingVerbosity {
     @param port Port to listen on
     @param rootDirectory Root directory for the server
     @param maxConnections Maximum number of connections to accept
+    @param requestLoggingVerbosity Verbosity at which to log the incoming requests, and their response code
 */
 struct HttpServerConfiguration {
     int port;
